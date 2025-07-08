bb977daafe428df64223d476548354d125880d6f
Mario Arias <mario@marioarias.org>
2025-07-08 22:40:39 +0800

    Fresh project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to monkey-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/monkey_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns monkey-clj.core-test
+  (:require [clojure.test :refer :all]
+            [monkey-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
